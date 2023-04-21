49634f7fe0eec768ce30284cc3fe2ea6fff7a939
Salif Shaikh <mohdammadsalif@gmail.com>
2023-04-21 11:56:07 +0530

    WeatherCard update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./css/App.css";
+import WeatherCard from "./components/WeatherCard";
 
 const App = () => {
   return (
     <React.Fragment>
-      <h1>Weather App</h1>
+      <WeatherCard />
     </React.Fragment>
   );
 };
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import "../css/WeatherCard.css";
 
-const WeatherCard = props => {
-  return <div></div>;
+const API_KEY = "a6bc244f057d38bd104009e5d359b3a7"; // Api Initiation key
+
+const WeatherCard = () => {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleCityChange = e => {
+    setCity(e.target.value);
+  };
+
+  const handleFormSubmit = async e => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      setWeatherData(response.data);
+      setError(null);
+    } catch (error) {
+      setError("City not found");
+      setWeatherData(null);
+    }
+  };
+
+  return (
+    <div className="weather-app">
+      <h1>Weather App</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={city}
+          onChange={handleCityChange}
+          placeholder="Enter a city name"
+        />
+        <button type="submit">Search</button>
+      </form>
+      {error && <div className="error-message">{error}</div>}
+      {weatherData && (
+        <div className="weather-data">
+          <h2>{weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp} &#8451;</p>
+          <p>Weather: {weatherData.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default WeatherCard;
